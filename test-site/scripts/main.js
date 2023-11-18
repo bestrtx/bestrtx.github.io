d004728a1f392483c511f823049f410dd4e2ef3e
Lu Junru <116940147+bestrtx@users.noreply.github.com>
2023-11-18 19:45:10 +0800

    Update main.js

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -8,27 +8,3 @@ myImage.onclick = function () {
     myImage.setAttribute("src", "images/monty-truth.jpg");
   }
 };
-
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h2");
-
-function setUserName() {
-  let myName = prompt("请输入你的名字。");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = "Mozilla 酷毙了，" + myName;
-  }
-}
-
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem("name");
-  myHeading.textContent = "Mozilla 酷毙了，" + storedName;
-}
-
-myButton.onclick = function () {
-  setUserName();
-};
